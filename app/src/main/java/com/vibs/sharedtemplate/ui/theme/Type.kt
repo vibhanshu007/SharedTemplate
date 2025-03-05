018b2c267e2e71f5d73cf76214f35b6b1cc138a6
Vibhanshu Shukla <vibhanshumail849@gmail.com>
2025-03-05 16:25:29 +0530

    Update Type.kt

--- app/src/main/java/com/vibs/sharedtemplate/ui/theme/Type.kt
+++ app/src/main/java/com/vibs/sharedtemplate/ui/theme/Type.kt
@@ -5,30 +5,97 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        color = arBlack
+    ),
+    displayMedium = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        color = arBlack
+    ),
+    displaySmall = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        color = arBlack
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        color = arBlack
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        color = arBlack
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        color = arBlack
+    ),
+    titleLarge = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+        color = arBlack
+    ),
+    titleMedium = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = arBlack
+    ),
+    titleSmall = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = arBlack
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ProximaNovaFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = arBlack
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = ProximaNovaFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        color = arBlack
+    ),
+    bodySmall = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        color = arBlack
+    ),
+    labelLarge = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = arBlack
+    ),
+    labelMedium = TextStyle(
+        fontFamily = ProximaNovaFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        color = arBlack
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ProximaNovaFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        color = arBlack
     )
-    */
 )
